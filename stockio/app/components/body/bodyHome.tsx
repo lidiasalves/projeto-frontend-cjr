0bad9d491877046647c4a70892e88bb017e17a57
maria <mariaemiraglia@gmail.com>
2025-11-08 18:37:04 -0300

    feat: home logada estático - header e navbar

---
+++ stockio/app/components/body/bodyHome.tsx
@@ -0,0 +1,16 @@
+import BarraPesquisa from "./barraPesquisa";
+
+const BodyHome = () => {
+    return (
+        <main className = "bg-auto min-h-screen px-8 py-12">
+            <div className="flex-row-reverse mb-10">
+                <BarraPesquisa/>
+            </div>
+
+
+        </main>
+        
+    )
+}
+
+export default BodyHome;
